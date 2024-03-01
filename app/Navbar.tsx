38255f7273b752108205d06aa3f80b222713f95c
lam-valamorgulis <80387108+lam-valamorgulis@users.noreply.github.com>
2024-03-01 14:18:39 +0000

    Navbar created

---
+++ app/Navbar.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <nav className='flex space-x-6 border-b mb-5 px-5 h-14'>
+      <link href='/'>
+        logo
+      </link>
+      <ul>
+        <li>Dashboard</li>
+        <li>Issues</li>
+      </ul>
+    </nav>
+  )
+}
+
+export default Navbar
